59a1013dd3637da162366a6516da8d24cbc1f0a4
Nathan Brown <34713147+N8Brown1@users.noreply.github.com>
2018-09-18 15:22:38 -0400

    Updated Javascript and HTML files
    Primary functionality is complete.

--- script.js
+++ script.js
@@ -1,49 +1,75 @@
 var body = document.getElementById('background'),
     gradientType = document.querySelector('#gradient-type'),
+    direction = document.querySelector('#gradient-direction'),
+    shape = document.querySelector('#gradient-shape'),
+    directionGroup = document.getElementsByClassName('direction'),
+    shapeGroup = document.getElementsByClassName('shape'),
     colorQuantity = document.querySelector('#color-quantity'),
-    colors = document.querySelectorAll('input[type=color]')
-    color1 = document.querySelector('#color1'),
-    color2 = document.querySelector('#color2'),
-    color3 = document.querySelector('#color3'),
-    color4 = document.querySelector('#color4');
+    colors = document.querySelectorAll('input[type=color]'),
+    code = document.getElementById('code'),
+    myColorIDs = [],
+    myColorValues = [],
+    myColorString = "",
+    myGradient = "",
+    myDirection = "",
+    myColorQuantity = "",
+    myShape = "";
+
+gradientType.addEventListener('change', generateBackground);
+direction.addEventListener('change', generateBackground);
+shape.addEventListener('change', generateBackground);
+colorQuantity.addEventListener('change', generateBackground);
 
 
-function getCurrent(){
-    var current = gradientType.value;
-    console.log(current);
-} 
+for (i=0; i<colors.length; i++){
+    colors[i].addEventListener('input', generateBackground);
+}
 
-
-// function getCurrentColorQuantity(){
-//     var current = colorQuantity.value;
-
-//     for (x=0; x<colors.length; x++){
-//         colors[x].disabled = true;
-//     }
-
-//     for (i=0; i<current; i++){
-//         colors[i].disabled = false;
-//     }
-//     console.log(current);
-// }
-
-function getCurrentColorQuantity(){
-    var current = colorQuantity.value;
+function generateBackground(){
+    myGradient = gradientType.value;
+    myDirection = direction.value + ", ";
+    myShape = shape.value + ", ";
+    myColorQuantity = colorQuantity.value;
+    myColorValues = [];
 
     for (x=0; x<colors.length; x++){
         colors[x].style.visibility = 'hidden';
     }
 
-    for (i=0; i<current; i++){
+    for (i=0; i<myColorQuantity; i++){
         colors[i].style.visibility = 'visible';
     }
-    console.log(current);
+
+    for (i=0; i<myColorQuantity; i++){
+        myColorValues.push(colors[i].value);
+    }
+    myColorString = myColorValues.join(', ');
+
+    if (myGradient === "linear-gradient" || myGradient === "repeating-linear-gradient"){
+        for (i=0; i<directionGroup.length; i++){
+            directionGroup[i].style.display = 'inline';
+        }
+        for (i=0; i<shapeGroup.length; i++){
+            shapeGroup[i].style.display = 'none';
+        }
+
+        body.style.background = myGradient + "(" + myDirection + myColorString + ")";
+        code.textContent = "background: " + body.style.background + ";";
+    }
+    else if (myGradient === "radial-gradient" || myGradient === "repeating-radial-gradient"){
+        for (i=0; i<shapeGroup.length; i++){
+            shapeGroup[i].style.display = 'inline';
+        }
+        for (i=0; i<directionGroup.length; i++){
+            directionGroup[i].style.display = 'none';
+        }
+        if (shape.value === "default"){
+            body.style.background = myGradient + "(" + myColorString + ")";
+            code.textContent = "background: " + body.style.background + ";";
+        }
+        else {
+            body.style.background = myGradient + "(" + myShape + myColorString + ")";
+            code.textContent = "background: " + body.style.background + ";";
+        }
+    }
 }
-
-
-var myGradType = gradientType.addEventListener('change', getCurrent),
-    myColorQuantity = colorQuantity.addEventListener('change', getCurrentColorQuantity);
-
-
-console.log(myGradType);
-console.log(myColorQuantity);
