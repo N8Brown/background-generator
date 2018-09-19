4344ced912990b3385ded878403a67978555800b
Nathan Brown <34713147+N8Brown1@users.noreply.github.com>
2018-09-19 11:56:52 -0400

    Completed Project
    Responsive CSS added. HTML cleaned up. Javascript condensed to one function.

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var body = document.getElementById('background'),
     gradientType = document.querySelector('#gradient-type'),
     direction = document.querySelector('#gradient-direction'),
     shape = document.querySelector('#gradient-shape'),
-    directionGroup = document.getElementsByClassName('direction'),
-    shapeGroup = document.getElementsByClassName('shape'),
+    directionGroup = document.getElementById('direction'),
+    shapeGroup = document.getElementById('shape'),
     colorQuantity = document.querySelector('#color-quantity'),
     colors = document.querySelectorAll('input[type=color]'),
     code = document.getElementById('code'),
@@ -15,6 +15,7 @@ var body = document.getElementById('background'),
     myColorQuantity = "",
     myShape = "";
 
+document.addEventListener('DOMContentLoaded', generateBackground);
 gradientType.addEventListener('change', generateBackground);
 direction.addEventListener('change', generateBackground);
 shape.addEventListener('change', generateBackground);
@@ -38,31 +39,24 @@ function generateBackground(){
 
     for (i=0; i<myColorQuantity; i++){
         colors[i].style.visibility = 'visible';
-    }
-
-    for (i=0; i<myColorQuantity; i++){
         myColorValues.push(colors[i].value);
     }
+
     myColorString = myColorValues.join(', ');
 
     if (myGradient === "linear-gradient" || myGradient === "repeating-linear-gradient"){
-        for (i=0; i<directionGroup.length; i++){
-            directionGroup[i].style.display = 'inline';
-        }
-        for (i=0; i<shapeGroup.length; i++){
-            shapeGroup[i].style.display = 'none';
-        }
-
+        directionGroup.classList.remove('hidden');
+        directionGroup.classList.add('split-box');
+        shapeGroup.classList.add('hidden');
+        shapeGroup.classList.remove('split-box');
         body.style.background = myGradient + "(" + myDirection + myColorString + ")";
         code.textContent = "background: " + body.style.background + ";";
     }
     else if (myGradient === "radial-gradient" || myGradient === "repeating-radial-gradient"){
-        for (i=0; i<shapeGroup.length; i++){
-            shapeGroup[i].style.display = 'inline';
-        }
-        for (i=0; i<directionGroup.length; i++){
-            directionGroup[i].style.display = 'none';
-        }
+        shapeGroup.classList.remove('hidden');
+        shapeGroup.classList.add('split-box');
+        directionGroup.classList.add('hidden');
+        directionGroup.classList.remove('split-box');
         if (shape.value === "default"){
             body.style.background = myGradient + "(" + myColorString + ")";
             code.textContent = "background: " + body.style.background + ";";
